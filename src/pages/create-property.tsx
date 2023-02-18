6d275e9e89e6bb4efa03f9719c8cd4228ca650e6
MD AWAIZ <awaizkhanmd@gmail.com>
2023-02-19 01:08:04 +0530

    new forms.tsx

--- src/pages/create-property.tsx
+++ src/pages/create-property.tsx
@@ -2,13 +2,26 @@ import { useState } from "react";
 import { useGetIdentity } from "@pankod/refine-core";
 import { FieldValue, useForm } from "@pankod/refine-react-hook-form";
 import { useNavigate } from "@pankod/refine-react-router-v6";
+import Form from "components/common/Form";
 const CreateProperty = () => {
   const navigate = useNavigate();
   const { data: user } = useGetIdentity();
   const [propertyImage, setpropertyImage] = useState({ name: "", url: "" });
   const { refineCore: { onFinish, formLoading }, register, handleSubmit } = useForm();
-
-  return <div>createProperty</div>;
+  const handleImageChange = () => { }
+  const onFinishHandler = () => { }
+  return (
+  
+      <Form
+          type="Create"
+          register={register}
+          onFinish={onFinish}
+          formLoading={formLoading}
+          handleSubmit={handleSubmit}
+          handleImageChange={handleImageChange}
+          onFinishHandler={onFinishHandler}
+          propertyImage={propertyImage}
+      />
+  );
 };
-
 export default CreateProperty;
